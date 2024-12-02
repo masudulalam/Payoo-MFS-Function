3fe944b899d77d11150267112d09476299bb7462
masudulalam <masuudulalam@gmail.com>
2024-12-03 02:07:23 +0600

    added transaction history in cash out section

--- js/cashOut.js
+++ js/cashOut.js
@@ -9,6 +9,15 @@ document.getElementById('btn-cash-out').addEventListener('click', function (even
         const newBalance = balance - cashOut;
 
         document.getElementById('account-balance').innerText = newBalance;
+
+        // add to transaction history
+        const div = document.createElement('div');
+        div.classList.add('bg-yellow-300');
+        div.innerHTML = `
+            <h4 class="text-2xl font-bold">Cash Out</h4>
+            <p>${cashOut} TK. Withdraw New Balance ${newBalance} TK.</p>
+        `
+        document.getElementById('transaction-container').appendChild(div);
     } else {
         alert ('Invalid Pin Number! Please Try Again.');
     }
