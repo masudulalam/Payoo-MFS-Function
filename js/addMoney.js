db13f378cb7775e633f07667cff0b8e426d92115
masudulalam <masuudulalam@gmail.com>
2024-12-02 19:26:28 +0600

    added add money event handler with using function

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,7 +1,18 @@
-console.log('addMoney.js loaded.');
+// console.log('addMoney.js loaded.');
 
 document.getElementById('btn-add-money').addEventListener('click', function(event) {
     event.preventDefault();
 
     console.log('add money button clicked.');
+
+    // getInputFieldValueById();
+    // const addMoney = getInputFieldValueById();
+    // console.log('add money value is:', addMoney);
+
+    // const addMoney = document.getElementById('add-money').value;
+    // const addMoneyNumber = parseFloat(addMoney);
+
+    const addMoney = getInputFieldValueById('input-add-money');
+    const pinNumber = getInputFieldValueById('input-pin-number');
+    console.log('add money with parameter:', addMoney, pinNumber);
 })
--- js/utilities.js
+++ js/utilities.js
@@ -4,8 +4,17 @@
  * 
  */
 
-console.log('utilities.js loaded.');
+// console.log('utilities.js loaded.');
 
-function getInputFieldValueById () {
-    
+// function getInputFieldValueById () {
+//     console.log('will get value by id.');
+
+//     const addMoney = document.getElementById('add-money').value;
+//     return addMoney;
+// }
+
+
+function getInputFieldValueById (id) {
+    const inputValue = document.getElementById(id).value;
+    return inputValue;
 }
