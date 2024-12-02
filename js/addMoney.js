c1f246d3cd327b733b8f00e3cc099560b2b67e55
masudulalam <masuudulalam@gmail.com>
2024-12-02 14:46:11 +0600

    added utilites.js

---
+++ js/addMoney.js
@@ -0,0 +1,7 @@
+console.log('addMoney.js loaded.');
+
+document.getElementById('btn-add-money').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    console.log('add money button clicked.');
+})
---
+++ js/utilities.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * Common Shared Functions here:
+ * 
+ */
+
+console.log('utilities.js loaded.');
+
+function getInputFieldValueById () {
+    
+}
